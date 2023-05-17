bc2c12f3a1a0c9cc478f6b4a2416942fb877c5f5
Lorenzo Benetollo <lorenzo.benetollo@gmail.com>
2023-05-17 18:07:44 +0200

    teal sample update

--- experiments/sample_trans1.py
+++ experiments/sample_trans1.py
@@ -10,8 +10,13 @@ def g(x: pt.Expr, y: pt.Expr) -> pt.Expr:
 
 @pt.Subroutine(pt.TealType.uint64)
 def f(x: pt.Expr) -> pt.Expr:
-    return g(pt.Add(x, pt.Int(4)), pt.Add(pt.Int(8), pt.Int(10)))
+    u1 = g(pt.Add(x, pt.Int(4)), pt.Add(pt.Int(8), pt.Int(10)))
+    u2 = g(pt.Add(x, pt.Int(4)), pt.Add(pt.Int(8), pt.Int(10)))
+    u3 = g(pt.Add(x, pt.Int(4)), pt.Add(pt.Int(8), pt.Int(10)))
+    return pt.Minus(u2, u3)
 
 @app.external
-def main(*, output: pt.abi.Uint64) -> pt.Expr:
-    return output.set(f(pt.Int(1)))
+def entry(*, output : pt.abi.Uint64) -> pt.Expr:
+    out = pt.abi.make(pt.abi.String)
+    return output.set(f(pt.Int(98)))
+    
